6034e714f3c44a4af6f7f796353e370a4d86a742
Shaik Mujeeb <mshaik58@gmail.com>
2017-10-13 15:04:32 +0100

    Refactored IRepository to IReadRepository and IWriteRespository
    Refactored IRepository to  be IReadRepository and IWriteRespository - The behind this  is to have  implementation for read and write funtionalities something silimar to CQRS pattern

---
+++ DevInterview Test/Interview/Interview/IReadRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interview
+{
+    public interface IReadRepository<T> where T : IStoreable
+    {
+        IEnumerable<T> All();
+
+        T FindById(IComparable id);
+    }
+}
---
+++ DevInterview Test/Interview/Interview/IWriteRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interview
+{
+    public interface IWriteRepository<T> where T : IStoreable
+    {
+       void Delete(IComparable id);
+        void Save(T item);
+    }
+
+}
