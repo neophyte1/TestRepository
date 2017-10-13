144e431fcee7189e6a0d19d0100d5cdd5ad4fd26
Shaik Mujeeb <mshaik58@gmail.com>
2017-10-13 10:15:32 +0100

    Added UnitTests project
    Added Interview.Tests project to the solution.

---
+++ DevInterview Test/Interview/Interview.Tests/InMemoryRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Interview.Tests
+{
+    [TestFixture]
+    class InMemoryRepositoryTests
+    {
+        private IRepository<User> _repository;
+
+        public InMemoryRepositoryTests()
+        {
+           // _repository  = new InMemoryRepository<User>();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void WhenSavedIsCalledShouldNewItem()
+        {
+            var user = new User()
+            {
+                Id = 1,
+                Name = "Shaik",
+                EmailAddress = "test@testmail.com"
+            };
+
+            _repository.Save(user);
+            
+        }
+    }
+
+    public  class User : IStoreable
+    {
+        public IComparable Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string EmailAddress { get; set; }
+    }
+}
